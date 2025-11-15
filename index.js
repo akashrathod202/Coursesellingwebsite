c70f6bbb17818d0e63851c69137de356607ae71a
Akash Rathod <akbhau2004@gmail.com>
2025-11-15 17:08:10 +0530

    Your message here

--- index.js
+++ index.js
@@ -7,6 +7,48 @@ app.get('/start',(req,res)=>{
     res.json("ok")
 })
 
-app.listen(3000,()=>{
-    console.log("✅ Server running on http://localhost:3000")
+
+app.post("user/signup",(req,res)=>{
+    try{
+       res.json({
+        message:"signup endpooint"
+       })
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+
+app.post("user/signin",(req,res)=>{
+    try{
+        res.json({
+            message: "sigin endpoint"
+        })
+    }
+    catch(error){
+        console.log("error")
+    }
+})
+
+
+app.post("user/Purchass ",(req,res)=>{
+    try{
+        res.json({
+            message: "sigin endpoint"
+        })
+    }
+    catch(error){
+        console.log("error")
+    }
+})
+
+app.get("/Courses",(req,res)=>{
+    try{
+        res.json({
+            message: "Courses"
+        })
+    }
+    catch(error){
+        console.log("error")
+    }
 })
